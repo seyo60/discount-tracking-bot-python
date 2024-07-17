4a71d90f0b620cb76700c13ff1519509e5cf64fe
seyo60 <105001109+seyo60@users.noreply.github.com>
2024-07-17 18:41:23 +0300

    Update discount-tracking-bot-with-python.py

--- discount-tracking-bot-with-python.py
+++ discount-tracking-bot-with-python.py
@@ -9,23 +9,23 @@ headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36"
 }
 
-def fiyat_kontrolu():
+def check_price():
     response = requests.get(url, headers=headers)
         
     soup = BeautifulSoup(response.text, 'html.parser')
     
     try:
-        fiyat = soup.find('span', class_='prc-dsc').text
-        yeni_fiyat = fiyat.replace(".", "").replace(",", ".")
-        yeni_fiyat = float(yeni_fiyat.replace(" TL", "").strip())
+        price = soup.find('span', class_='prc-dsc').text
+        new_price = price.replace(".", "").replace(",", ".")
+        new_price = float(new_price.replace(" TL", "").strip())
 
-        if yeni_fiyat < 1400:
-            email_gonder(yeni_fiyat)
+        if new_price < 1400:
+            send_email(new_price)
 
     except Exception as e:
-        print("Fiyat kontrolünde bir hata oluştu:", e)
+        print("There was an error in the price check:", e)
 
-def email_gonder(yeni_fiyat):
+def send_email(new_price):
 
     server = smtplib.SMTP('smtp.gmail.com', 587)
     server.ehlo()
@@ -35,14 +35,14 @@ def email_gonder(yeni_fiyat):
 
     server.login('trial20022@gmail.com', '123456')
 
-    baslik = 'Fiyat dususu!'
-    uyari = f'Bekledigin fiyatin altina dustu! {yeni_fiyat} TL! Bu linkten kontrol et: {url}'
+    header = 'Price drop!'
+    warning = f'The price you expected fell below! {new_price} TL! Check it out from this link: {url}'
 
-    icerik = f"Subject: {baslik}\n\n{uyari}"
+    msgcontent = f"Subject: {header}\n\n{warning}"
 
-    server.sendmail('trial2002@gmail.com','trial2001@gmail.com',icerik)
+    server.sendmail('trial2002@gmail.com','trial2001@gmail.com',msgcontent)
 
-    print('Mesaj gonderildi')
+    print('Message sent')
 
     server.quit()
 
